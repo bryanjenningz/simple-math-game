ce35271a255a80b5e431ff8c80cda4e88d9a9528
Bryan Jennings <bryanjenningz@gmail.com>
2020-01-20 00:31:40 -0800

    Updates

---
+++ precache-manifest.91e03ceaee7fb2dd4c095f30e7986602.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/simple-math-game/favicon.ico"
+  },
+  {
+    "revision": "e1280b3d208af5ddc9c5a6ea2dc746fc",
+    "url": "/simple-math-game/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/simple-math-game/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/simple-math-game/manifest.json"
+  },
+  {
+    "revision": "502bfa9bc9e775d84a35",
+    "url": "/simple-math-game/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "502bfa9bc9e775d84a35",
+    "url": "/simple-math-game/static/js/main.502bfa9b.chunk.js"
+  },
+  {
+    "revision": "648a868c3fe20f7462a6",
+    "url": "/simple-math-game/static/js/runtime~main.648a868c.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/simple-math-game/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
